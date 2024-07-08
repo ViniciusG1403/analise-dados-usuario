2fe77c65b3bda9912dc77b074d3705d1f860e8a2
Vinicius <vinicius.prado@nexuscloud.com.br>
2024-07-07 21:09:59 -0300

    First commit

---
+++ src/main/java/org/pradosoftwares/user_data_analysis/UserDataAnalysisApplication.java
@@ -0,0 +1,13 @@
+package org.pradosoftwares.user_data_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDataAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pradosoftwares/user_data_analysis/UserDataAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pradosoftwares.user_data_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
